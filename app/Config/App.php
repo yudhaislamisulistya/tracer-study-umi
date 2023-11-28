9afadb10b5e8ff9dc3b824168748789158089881
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2023-11-29 00:07:24 +0700

    update

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://alumni.umi.ac.id/';
+    public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------
