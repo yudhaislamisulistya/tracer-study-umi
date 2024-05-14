c7adae699399c488fe89ec4af300563df9e510bb
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2024-05-14 09:17:27 +0700

    feat: change sidebar mode

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -52,7 +52,7 @@ License: You must have a valid license purchased only from themeforest(the above
         .aside {
             width: 250px;
             /* Width of the sidebar */
-            position: fixed;
+            position: relative;
             /* Fixed position to stay visible */
             top: 0;
             left: 0;
@@ -65,6 +65,10 @@ License: You must have a valid license purchased only from themeforest(the above
             /* High z-index to stay on top */
             transition: transform 0.3s ease;
             /* Smooth transition for toggling */
+        }
+
+        #kt_aside {
+            margin-top: 50px;
         }
 
         /* Hide sidebar by default on small screens */
@@ -273,16 +277,12 @@ if (session()->get('STATUS') == 'alumni') {
 
                 <!--begin::Wrapper-->
                 <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
-                    <!--begin::Header-->
                     <div id="kt_header" class="header  header-fixed ">
-                        <!--begin::Container-->
                         <div class=" container  d-flex align-items-stretch justify-content-between">
-                            <!--begin::Left-->
                             <div class="d-none d-lg-flex align-items-center mr-3">
-                                <!--begin::Aside Toggle-->
                                 <button class="btn btn-icon aside-toggle ml-n3 mr-10" id="kt_aside_desktop_toggle">
                                     <span class="svg-icon svg-icon-xxl svg-icon-dark-75">
-                                        <!--begin::Svg Icon | path:assets/media/svg/icons/Text/Align-left.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                                 <rect x="0" y="0" width="24" height="24" />
                                                 <rect fill="#000000" opacity="0.3" x="4" y="5" width="16" height="2" rx="1" />
@@ -290,44 +290,29 @@ if (session()->get('STATUS') == 'alumni') {
                                                 <path d="M5,9 L13,9 C13.5522847,9 14,9.44771525 14,10 C14,10.5522847 13.5522847,11 13,11 L5,11 C4.44771525,11 4,10.5522847 4,10 C4,9.44771525 4.44771525,9 5,9 Z M5,17 L13,17 C13.5522847,17 14,17.4477153 14,18 C14,18.5522847 13.5522847,19 13,19 L5,19 C4.44771525,19 4,18.5522847 4,18 C4,17.4477153 4.44771525,17 5,17 Z" fill="#000000" />
                                             </g>
                                         </svg>
-                                        <!--end::Svg Icon--></span> </button>
-                                <!--end::Aside Toggle-->
-
-                                <!--begin::Logo-->
+                                    </span>
+                                </button>
                                 <a href="<?php base_url('/dashboard') ?>">
                                     <img alt="Logo" src="<?= base_url('assets/media/logos/umi.png') ?>" class="logo-sticky max-h-35px" />
                                 </a>
-                                <!--end::Logo-->
-
-
                             </div>
-                            <!--end::Left-->
-
-                            <!--begin::Topbar-->
                             <div class="topbar">
-
-                                <!--begin::User-->
                                 <div class="topbar-item mr-4">
                                     <div class="btn btn-icon btn-sm btn-clean btn-text-dark-75" id="kt_quick_user_toggle">
                                         <span class="svg-icon svg-icon-lg">
-                                            <!--begin::Svg Icon | path:assets/media/svg/icons/General/User.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                                     <polygon points="0 0 24 0 24 24 0 24" />
                                                     <path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
                                                     <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
                                                 </g>
                                             </svg>
-                                            <!--end::Svg Icon--></span>
+                                        </span>
                                     </div>
                                 </div>
-                                <!--end::User-->
-
                             </div>
-                            <!--end::Topbar-->
                         </div>
-                        <!--end::Container-->
                     </div>
-                    <!--end::Header-->
                 <?php
             } else if (session()->get('STATUS') == 'admin') { ?>
 
@@ -367,7 +352,7 @@ if (session()->get('STATUS') == 'alumni') {
                             <div class="d-flex flex-row flex-column-fluid page">
                                 <!-- begin::Aside-->
                                 <!--begin::Aside-->
-                                <div class="aside aside-left d-flex flex-column flex-row-auto" id="kt_aside">
+                                <div class="d-flex flex-column flex-row-auto" id="kt_aside">
                                     <!--begin::Aside Menu-->
                                     <div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
                                         <!--begin::Menu Container-->
@@ -647,16 +632,12 @@ if (session()->get('STATUS') == 'alumni') {
 
                                 <!--begin::Wrapper-->
                                 <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
-                                    <!--begin::Header-->
                                     <div id="kt_header" class="header  header-fixed ">
-                                        <!--begin::Container-->
-                                        <div class=" container  d-flex align-items-stretch justify-content-between">
-                                            <!--begin::Left-->
+                                        <div class=" container-fluid  d-flex align-items-stretch justify-content-between">
                                             <div class="d-none d-lg-flex align-items-center mr-3">
-                                                <!--begin::Aside Toggle-->
-                                                <button class="btn btn-icon aside-toggle ml-n3 mr-10" id="kt_aside_desktop_toggle">
+                                                <!-- <button class="btn btn-icon aside-toggle ml-n3 mr-10" id="kt_aside_desktop_toggle">
                                                     <span class="svg-icon svg-icon-xxl svg-icon-dark-75">
-                                                        <!--begin::Svg Icon | path:assets/media/svg/icons/Text/Align-left.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                                             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                                                 <rect x="0" y="0" width="24" height="24" />
                                                                 <rect fill="#000000" opacity="0.3" x="4" y="5" width="16" height="2" rx="1" />
@@ -664,44 +645,29 @@ if (session()->get('STATUS') == 'alumni') {
                                                                 <path d="M5,9 L13,9 C13.5522847,9 14,9.44771525 14,10 C14,10.5522847 13.5522847,11 13,11 L5,11 C4.44771525,11 4,10.5522847 4,10 C4,9.44771525 4.44771525,9 5,9 Z M5,17 L13,17 C13.5522847,17 14,17.4477153 14,18 C14,18.5522847 13.5522847,19 13,19 L5,19 C4.44771525,19 4,18.5522847 4,18 C4,17.4477153 4.44771525,17 5,17 Z" fill="#000000" />
                                                             </g>
                                                         </svg>
-                                                        <!--end::Svg Icon--></span> </button>
-                                                <!--end::Aside Toggle-->
-
-                                                <!--begin::Logo-->
+                                                    </span>
+                                                </button> -->
                                                 <a href="<?php base_url('/dashboard') ?>">
                                                     <img alt="Logo" src="<?= base_url('assets/media/logos/umi.png') ?>" class="logo-sticky max-h-35px" />
                                                 </a>
-                                                <!--end::Logo-->
-
-
                                             </div>
-                                            <!--end::Left-->
-
-                                            <!--begin::Topbar-->
                                             <div class="topbar">
-
-                                                <!--begin::User-->
                                                 <div class="topbar-item mr-4">
                                                     <div class="btn btn-icon btn-sm btn-clean btn-text-dark-75" id="kt_quick_user_toggle">
                                                         <span class="svg-icon svg-icon-lg">
-                                                            <!--begin::Svg Icon | path:assets/media/svg/icons/General/User.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                                                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                                                     <polygon points="0 0 24 0 24 24 0 24" />
                                                                     <path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
                                                                     <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
                                                                 </g>
                                                             </svg>
-                                                            <!--end::Svg Icon--></span>
+                                                        </span>
                                                     </div>
                                                 </div>
-                                                <!--end::User-->
-
                                             </div>
-                                            <!--end::Topbar-->
                                         </div>
-                                        <!--end::Container-->
                                     </div>
-                                    <!--end::Header-->
                                 <?php } else { ?>
 
                                     <body id="kt_body" class="quick-panel-right demo-panel-right offcanvas-right header-fixed header-mobile-fixed aside-enabled aside-static page-loading">
@@ -904,9 +870,9 @@ if (session()->get('STATUS') == 'alumni') {
                                                     <!--end::Header-->
                                                 <?php } ?>
 
-<script>
-function toggleSidebar() {
-    const sidebar = document.getElementById('kt_aside');
-    sidebar.style.transform = sidebar.style.transform === 'translateX(0px)' ? 'translateX(-100%)' : 'translateX(0px)';
-}
-</script>
+                                                <script>
+                                                    function toggleSidebar() {
+                                                        const sidebar = document.getElementById('kt_aside');
+                                                        sidebar.style.transform = sidebar.style.transform === 'translateX(0px)' ? 'translateX(-100%)' : 'translateX(0px)';
+                                                    }
+                                                </script>
