53e00b3271cb2f325b3f5f119b09d5e838036ac6
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2024-05-20 00:19:13 +0700

    refactor: remove unused delete_admin_berita method

--- app/Controllers/BeritaController.php
+++ app/Controllers/BeritaController.php
@@ -128,8 +128,6 @@ class BeritaController extends BaseController
 
     public function delete_admin_berita()
     {
-    public function delete_admin_berita()
-    {
         try {
             $hapusId = $this->request->getPost('hapusId');
             $query = $this->ModelBerita->delete_data($hapusId);
